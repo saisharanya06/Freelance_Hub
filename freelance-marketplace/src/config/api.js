c6fecbf87ae8fbd28c06604c66b407ca098b2af3
saisharanya <saisharanya747721@gmail.com>
2026-01-18 23:22:16 +0530

    Fix: Uncomment api.js - entire file was commented out

--- freelance-marketplace/src/config/api.js
+++ freelance-marketplace/src/config/api.js
@@ -1,37 +1,37 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-// if (!API_BASE_URL) {
-//   throw new Error("❌ VITE_API_URL not defined");
-// }
+if (!API_BASE_URL) {
+  throw new Error("VITE_API_URL not defined");
+}
 
-// const api = axios.create({
-//   baseURL: API_BASE_URL,
-//   withCredentials: false,
-// });
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: false,
+});
 
-// api.interceptors.request.use((config) => {
-//   const token = sessionStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+api.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-// api.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     console.error(
-//       "API Error:",
-//       err.response?.status,
-//       err.response?.data || err.message
-//     );
-//     return Promise.reject(err);
-//   }
-// );
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    console.error(
+      "API Error:",
+      err.response?.status,
+      err.response?.data || err.message
+    );
+    return Promise.reject(err);
+  }
+);
 
-// export default api;
+export default api;
 
 import axios from "axios";
 
