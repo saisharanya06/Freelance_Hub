27ff4e5bcda1a15ff359efbd5a0630918e987f49
saisharanya <saisharanya747721@gmail.com>
2026-01-18 23:19:13 +0530

    modification

--- freelance-marketplace/src/config/api.js
+++ freelance-marketplace/src/config/api.js
@@ -1,14 +1,42 @@
+// import axios from "axios";
+
+// const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+// if (!API_BASE_URL) {
+//   throw new Error("❌ VITE_API_URL not defined");
+// }
+
+// const api = axios.create({
+//   baseURL: API_BASE_URL,
+//   withCredentials: false,
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = sessionStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// api.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     console.error(
+//       "API Error:",
+//       err.response?.status,
+//       err.response?.data || err.message
+//     );
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default api;
+
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
-
-if (!API_BASE_URL) {
-  throw new Error("❌ VITE_API_URL not defined");
-}
-
 const api = axios.create({
-  baseURL: API_BASE_URL,
-  withCredentials: false,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
@@ -18,17 +46,5 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    console.error(
-      "API Error:",
-      err.response?.status,
-      err.response?.data || err.message
-    );
-    return Promise.reject(err);
-  }
-);
 
 export default api;
