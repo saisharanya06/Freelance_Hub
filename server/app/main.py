b82b06826b1162ad5b9dccd6e3ca57f991c9cbf3
saisharanya <saisharanya747721@gmail.com>
2026-01-19 19:46:59 +0530

    modification

--- server/app/main.py
+++ server/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(title="Freelance Projects API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://incredible-salamander-a39aca.netlify.app"],
+        "https://freelancehub-a39aca.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
